5ecd76ff61ece1be120c618d9ea38edee3d1e447
Nicolas <nicolasofus@gmail.com>
2018-03-26 15:33:59 +0100

    app

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const opn = require('opn');
 
-app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function(req, res) {
     res.json({ message: 'BattleU' });
