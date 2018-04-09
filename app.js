522c666f324364f7b97bfbf8b92f662cbe2aa310
Nicolas <nicolasofus@gmail.com>
2018-04-09 16:11:53 +0100

    push

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const opn = require('opn');
-const bodyparser = require('bodyparser');
+const bodyparser = require('body-parser');
 
 app.use(require('express-session')({ resave: false, saveUninitialized: false, secret: 'a secret' }));
 
